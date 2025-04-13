ca01b971a6cabb75c5c60512402395981992cdfa
David Felix <davidffelix@hotmail.com>
2025-04-13 16:03:52 -0300

    feat: add initial web crawler to fetch proxy list HTML

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string url = "https://proxyservers.pro/proxy/list/order/updated/order_dir/desc";
+
+        using HttpClient client = new HttpClient();
+        string html = await client.GetStringAsync(url);
+
+        Console.WriteLine(html);
+    }
+}
