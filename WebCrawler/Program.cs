27ccd2f5feca39663ef73159b0a4d70064566c1e
David Felix <davidffelix@hotmail.com>
2025-04-15 15:48:32 -0300

    refactor: migrate to class-based Program with service orchestration

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
-
-class Program
+﻿public class Program
 {
-    static async Task Main()
+    public static async Task Main()
     {
-        string url = "https://proxyservers.pro/proxy/list/order/updated/order_dir/desc";
-
-        using HttpClient client = new HttpClient();
-        string html = await client.GetStringAsync(url);
-        HtmlDocument doc = new HtmlDocument();
-        doc.LoadHtml(html);
-
-        Console.WriteLine(html);
+        IProxyService service = new ProxyService();
+        var proxies = await service.FetchProxiesMultithreadedAsync();
+        await service.SaveProxiesAsJsonAsync(proxies);
     }
 }
